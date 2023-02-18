6f5939751f8c2772d2bd69fcf3863fa8ac08b8e5
SCALEFOCUS\vladimir.conev <vladimir.conev@scalefocus.com>
2023-02-18 21:47:55 +0100

    initial commit

---
+++ src/main/java/com/example/testcontainers/sandbox/TestcontainersSandboxApplication.java
@@ -0,0 +1,12 @@
+package com.example.testcontainers.sandbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestcontainersSandboxApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TestcontainersSandboxApplication.class, args);
+  }
+}
---
+++ src/test/java/com/example/testcontainers/sandbox/TestcontainersSandboxApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.testcontainers.sandbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@SpringBootTest
+@ActiveProfiles("test")
+@Testcontainers(disabledWithoutDocker = true)
+class TestcontainersSandboxApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
